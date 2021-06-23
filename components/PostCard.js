15476da400f887b0dde34f5a938c10f99e4f4ad3
ygpalta <ygpalta@gmail.com>
2021-06-24 01:12:13 +0530

    image

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import Image from 'next/Image'
+import Image from 'next/image'
 import React, { ReactElement, useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
