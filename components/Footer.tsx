e69dd540e77670b0e6ef81efeca921baa9864098
ygpalta <ygpalta@gmail.com>
2021-06-29 18:57:36 +0530

    About and Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,176 @@
+import React, { ReactElement } from 'react'
+
+
+function Footer(): ReactElement {
+    return (
+        <footer className="relative bg-red-700 pt-8 pb-6">
+        <div
+          className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
+          style={{ height: "80px" }}
+        >
+          <svg
+            className="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              className="text-red-700 fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
+        <div className="container mx-auto px-4">
+          <div className="flex flex-wrap">
+            <div className="w-full lg:w-6/12 px-4">
+              
+              <h4 className="text-3xl font-semibold text-white mt-2 ">
+                Let&apos;s keep in touch!
+              </h4>
+              <div className="text-white px-4 py-2">
+                <h5 className="fas fa-map-marker-alt text-lg mt-0 text-white">
+                &nbsp;&nbsp;405 The West Mall
+                </h5>
+                <h5 className="text-lg mt-0 font-semibold">
+                &emsp;&nbsp;Unit 910
+                </h5>
+                <h5 className="text-lg mt-0 font-semibold">
+                &emsp;&nbsp;Etobicoke
+                </h5>  
+                <h5 className="text-lg mt-0 font-semibold">
+                &emsp;&nbsp;Toronto, ON
+                </h5>
+                <h5 className="text-lg mt-0 mb-1 font-semibold">
+                &emsp;&nbsp;Canada
+                </h5>
+
+                <h5 className="fas fa-envelope text-lg mt-0">
+                &nbsp;&nbsp;buildingblocksimmigration@gmail.com
+                </h5><br/>
+
+                <h5 className="fas fa-phone text-lg mt-0">
+                &nbsp;&nbsp;+1 4166199215
+                </h5>
+              </div>
+
+              <div className="mt-6">
+                <button
+                  className="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                  type="button"
+                >
+                  <i className="flex fab fa-twitter"></i>
+                </button>
+                <button
+                  className="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                  type="button"
+                >
+                  <i className="flex fab fa-facebook-square"></i>
+                </button>
+                <button
+                  className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                  type="button"
+                >
+                  <i className="flex fab fa-instagram"></i>
+                </button>
+                <button
+                  className="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                  type="button"
+                >
+                  <i className="flex fab fa-linkedin"></i>
+                </button>
+              </div>
+            </div>
+            <div className="w-full lg:w-6/12 px-4 mt-4">
+              <div className="flex flex-wrap items-top mb-6">
+                <div className="w-full lg:w-4/12 px-4 ml-auto">
+                  <span className="block uppercase text-white text-sm font-semibold mb-2">
+                    Useful Links
+                  </span>
+                  <ul className="list-unstyled">
+                    <li>
+                      <a className="text-white hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="/about">About Us
+                      </a>
+                    </li>
+                    <li>
+                      <a className="text-white hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="/blog">Blog
+                      </a>
+                    </li>
+                    <li>
+                      <a className="text-white hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="/assesment">Free Assesment
+                      </a>
+                    </li>
+                    <li>
+                      <a className="text-white hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="/book-consultation">Book A Consultation
+                      </a>
+                    </li>
+                    <li>
+                      <a className="text-white hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="/contact">Contact Us
+                      </a>
+                    </li>
+
+                  </ul>
+                </div>
+                <div className="w-full lg:w-4/12 px-4">
+                  <span className="block uppercase text-white text-sm font-semibold mb-2">
+                    Services
+                  </span>
+                  <ul className="list-unstyled">
+                    <li>
+                      <a className="text-white hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="/migrate-canada">Migrate
+                    </a>
+                    </li>
+                    <li>
+                      <a className="text-white hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="/study-in-canada">Study
+                      </a>
+                    </li>
+                    <li>
+                      <a className="text-white hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="/work-in-canada">Work
+                      </a>
+                    </li>
+                    <li>
+                      <a className="text-white hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="/business-in canada">Business
+                      </a>
+                    </li>
+                    <li>
+                      <a className="text-white hover:text-gray-900 font-semibold block pb-2 text-sm"
+                        href="/visit-canada">Visit
+                      </a>
+                    </li>
+                  </ul>
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr className="my-6 border-gray-400" />
+          <div className="flex flex-wrap items-center md:justify-between justify-center">
+            <div className="w-full md:w-8/12 px-4 mx-auto text-center">
+              <div className="text-sm text-white font-semibold py-1">
+                Copyright © {new Date().getFullYear()}{" "}Building Blocks Immigration- All Rights Reserved || Site Developed by{" "}
+                <a
+                  href="https://www.linkedin.com/in/ygpalta/"
+                  className="text-white hover:text-gray-900"
+                >
+                  Y.G. Palta
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
+}
+
+export default Footer
---
+++ pages/about.js
@@ -0,0 +1,121 @@
+import  Navbar from '../components/Navbar.tsx';
+import  Footer from '../components/Footer.tsx';
+
+
+export default function Page () {
+    return (
+        <div>
+            <Navbar transparent={false} />
+            <div className="hero-image">
+                <div className="hero-text">
+                <h1 className="text-6xl md:text-8xl uppercase tracking-widest">About Us</h1>
+                
+                
+                </div>
+            </div>
+            <About />
+            <Director />
+            <Footer />
+        </div>
+    )
+}
+
+
+
+const About = () => <section className=" bg-gray-300 -mt-48">
+  <div className="container mx-auto px-4">
+    <div className="flex flex-wrap">
+
+      <div className="w-full px-4 ">
+        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+          <div className="px-10 py-10 flex-auto">
+
+            <h2 className="text-3xl font-semibold">
+              Who We Are
+              </h2>
+            <p className="text-lg mt-2 mb-5">
+            Building Blocks Immigration is a Canadian Company that offers professional consultation services in all immigration matters. We are physically located in Toronto, Ontario, we offer in-person as well as virtual consultation that allows us to work with you as per your preference and convenience. We offer services in English, Hindi, Punjabi, and Urdu.
+            </p>
+         
+
+            <h2 className="text-3xl mt-5 font-semibold">
+              Why Us
+              </h2>
+            <p className="text-lg mt-2 mb-5">At Building Blocks Immigration, we walk the journey with you towards building your future from initial consultation to post settlement. We pride ourselves in helping our clients achieve their goals by walking them through various programs and options suited to their needs, made available by the Immigration, Refugees and Citizenship Canada. We turn your dreams into reality by listening to you, understanding your case, and recommending the best pathway that fits your education, work experience, status, and ambitions in Canada. By retaining us for your representation, you are bringing years of experience, depth of knowledge and professionalism to your correspondences with immigration officials.
+            </p>
+            <hr />
+
+
+            <h2 className="text-3xl mt-5 font-semibold">
+              Our Mission
+            </h2>
+            <p className="text-lg mt-2 mb-5">To provide a seamless and stress-free journey to Canada.</p>
+            
+
+            <h2 className="text-3xl mt-5 font-semibold">
+              Our Vision
+            </h2>
+            <p className="text-lg mt-2 mb-5">Helping people all over the world fulfil their dream of starting a new life in Canada.</p>
+
+
+            <h2 className="text-3xl mt-5 font-semibold">
+              Our Values
+            </h2>
+            <div className="text-lg mt-2 px-8 mb-5">
+
+                <dl>
+                    <dt><strong>Excellence: </strong></dt>
+                    <dd>-&ensp;&ensp;&ensp;We strive to provide best customer service and care through continuous improvement. We work hard to help our clients achieve their goals. We keep our clients informed about the changes in policies and help them make informed decisions.</dd>
+
+                    <dt><strong>Integrity: </strong></dt>
+                    <dd>-&ensp;&ensp;&ensp; We strive for highest level of integrity. Our processes and procedures are simple, efficient, and transparent. We inform our clients of their rights and options, letting them know the programs they qualify for and their chances at success</dd>
+
+                    <dt><strong>Respect: </strong></dt>
+                    <dd>-&ensp;&ensp;&ensp;We treat all our clients with dignity and respect by offering personalized services and one-on-one guidance to every case. We always maintain trust and confidentiality.</dd>
+                    </dl>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+    </div>
+    </section>
+
+const Director = () => <section className="pb-20 bg-gray-300">
+<div className="container mx-auto px-4">
+  <div className="flex flex-wrap">
+
+    <div className="w-full px-4 ">
+      <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+        <div className="px-10 py-10 flex-auto">
+       
+
+          <h2 className="text-3xl mt-5 font-semibold">
+            Director
+            </h2>
+            <div className="text-lg mt-2 mb-5">
+                <p>
+                Jyoti Palta, the founder, and director of Building Blocks Immigration is a Regulated Canadian Immigration Consultant (RCIC) and a member in good standing with the Immigration Consultants of Canada Regulatory Council (ICCRC).
+
+                </p><p>She is also a member of the Canadian Association of Professional Immigration Consultants (CAPIC).
+
+                </p><br/><p>As a landed immigrant herself, Jyoti knows how difficult it can be to start a new life in a new country with new hopes and dreams. She knows it is not easy to trust someone when your ambitions, dreams and goals are at stake. Immigration process can be complex and confusing and sometimes it feels like there is no light at the end of the tunnel. It is her experience with the process that led her to pursue a career in immigration, she aims to make the process simpler and easy to understand for people like her who dream of a new life in Canada.
+
+                </p><br/><p>Under her guidance we at Building Blocks Immigration, make sure you get personalized services, we listen to you and offer you advise based on your credentials and dreams. We pick the right building blocks for your strong foundation.
+                </p>
+            </div>
+
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+  </div>
+  </section>
+
+    
